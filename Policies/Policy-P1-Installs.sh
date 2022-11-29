e1b821b65a830aaeba72f232a4517745a0c5a676
malinkyzubr <ahumanbeing189@gmail.com>
2022-11-29 09:25:11 -0800

    fixed file id error

--- Policies/Policy-P1-Installs.sh
+++ Policies/Policy-P1-Installs.sh
@@ -4,25 +4,41 @@ sudo apt-get -y upgrade
 sudo apt-get -y install auditd && auditctl -e 1
 
 
+function isinstalled()
+{
+	dpkg -s $1 &> $1
+
+	if [ $? -eq 0 ]; then
+	   	val=1
+	else
+		val=0
+	fi
+	echo $val
+}
+
 function check_service()
 {	
-	which $1 << service_path
-service_path
+	service_path=$(which $service)
+	installed=$(isinstalled $service)
 	
-	echo "[+] proceeding to $2 $1 at $service_path"
-	
-	if [ $2 == "install" ] && [ -z $service_path ]; then
+	if [ $2 == "install" ] && [ $installed == 0 ]; then
+		echo "[+] $1 not found, installing now"
 		sudo apt -y install $service
-	elif [ $2 == "delete" ] && [ -n $service_path ]; then
-		sudo apt -y autoremove $service
+	elif [ $2 == "install" ] && [ $installed == 1 ]; then
+		echo "[-] $1 already installed"
+	elif [ $2 == "delete" ] && [ $installed == 0 ]; then
+		echo "[-] Tried to remove $1 but $1 could not be found."
+	elif [ $2 == "delete" ] && [ $installed == 1 ]; then
+		echo "[+] Illegal program $1 found, deleting now"
+		sudo apt -y autoremove --purge $service
 	else
-		echo "[-] program $1 does not exist"
+		echo "[-] UNKNOWN ERROR"
 	fi	
 }
 
 #service checks
-blacklisted=("nmap" "zenmap" "apache2" "nginx" "lighttpd" "wireshark" "tcpdump" "netcat-traditional" "nikto" "ophcrack" "libpam-pwquality" "telnet")
-whitelisted=("chkrootkit" "rkhunter" "ufw" "libpam-cracklib" "fail2ban")
+blacklisted=("nmap" "zenmap" "apache2" "nginx" "lighttpd" "wireshark" "tcpdump" "netcat-traditional" "nikto" "ophcrack" "libpam-pwquality" "telnet" "samba")
+whitelisted=("chkrootkit" "rkhunter" "ufw" "libpam-cracklib" "fail2ban" "openssh-server")
 
 for service in "${blacklisted[@]}"
 do 
@@ -38,7 +54,8 @@ done
 
 #remove all samba, music, mp4, targz, tgz, zip, and deb files
 sudo apt-get remove .*samba.* .*smb.* .*mp4.* .*tar.gz.* .*tgz.* .*zip.* .*deb.*
-sudo apt-get autoremove --purge samba
 
 echo "[+] all installs and uninstalls complete. Ready to restart.\n"
 read -n 1 -r -s -p "Press any key to continue..." 
+
+sudo reboot now
--- test.sh
+++ test.sh
@@ -1,33 +1,22 @@
 #!/bin/bash
 
-function ufw_access()
-{
-	local -n array=$1
-	for i in "${array[@]}"
-	do 
-		echo "$i $2"
-	done
-}
 
-deny=(1 2 3) 
-ufw_access deny "deny"
 
-function funky()
+
+#!/bin/bash
+
+function isinstalled()
 {
-	local -n string=$1
-	local -n operation=$2
-	echo $operation
-	if [ -z $operation string ]
-	then
-		echo "true"
+	dpkg -s $1 &> $1
+
+	if [ $? -eq 0 ]; then
+	   	val=1
+	else
+		val=0
 	fi
+	echo $val
 }
-funky "hi" "z"
 
-
-service="ufw"
-which $service << service_path
-service_path
-
-echo $service_path
+val=$(isinstalled "wireshark")
+echo $val
 
